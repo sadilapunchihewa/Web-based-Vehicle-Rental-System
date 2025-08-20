f095fe42cb97ab0694d2662fb5a1c1e6a2c3d2a6
IT24102297 <it24102297@my.sliit.lk>
2025-08-20 09:54:09 +0530

    Initialize the backend and frontend file structure

---
+++ src/main/java/com/vehicle_mangement_system/backend/models/User.java
@@ -0,0 +1,11 @@
+package com.vehicle_mangement_system.backend.models;
+
+public class User {
+    public String userName;
+    public String friendlyName;
+
+    public User(String userName, String friendlyName) {
+        this.userName = userName;
+        this.friendlyName = friendlyName;
+    }
+}
---
+++ src/test/java/com/vehicle_mangement_system/backend/VehicalRentalSystemBackendTests.java
@@ -0,0 +1,13 @@
+package com.vehicle_mangement_system.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicalRentalSystemBackendTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
