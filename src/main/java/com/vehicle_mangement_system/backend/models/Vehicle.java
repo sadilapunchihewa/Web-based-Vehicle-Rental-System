e65818fb978c824caa2f433901121e43bc1512f2
IT24102308 <it24102308@my.sliit.lk>
2025-10-01 11:45:38 +0530

    Committing Vehicle Management IT24102308

---
+++ src/main/java/com/vehicle_mangement_system/backend/models/Vehicle.java
@@ -0,0 +1,143 @@
+package com.vehicle_mangement_system.backend.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String brand;
+
+    private String model;
+
+    private String type;
+
+    private Integer year;
+
+    private Integer mileage;
+
+
+    private Integer pricing;
+
+
+    private String fuelType;
+
+    private Integer seatingCapacity;
+
+    private String description;
+
+    private String features;
+
+    private String status;
+
+    @Lob
+    private String imageBase64;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Integer getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(Integer mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public Integer getSeatingCapacity() {
+        return seatingCapacity;
+    }
+
+    public void setSeatingCapacity(Integer seatingCapacity) {
+        this.seatingCapacity = seatingCapacity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(String features) {
+        this.features = features;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getImageBase64() {
+        return imageBase64;
+    }
+
+    public void setImageBase64(String imageBase64) {
+        this.imageBase64 = imageBase64;
+    }
+
+    public void setPricing(Integer pricing) {
+        this.pricing = pricing;
+    }
+
+
+    public Integer getPricing() {
+        return pricing;
+    }
+}
